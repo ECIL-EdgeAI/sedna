bcf23dec42ac90c9884f940b4347fc364c5555ff
luosiqi <1587295470@qq.com>
2022-10-27 11:33:31 +0800

    Unite edge worker

--- examples/lifelong_learning/robot_dog_delivery/robo_detection/run.sh
+++ examples/lifelong_learning/robot_dog_delivery/robo_detection/run.sh
@@ -5,6 +5,7 @@ set -e
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/home/deep_msgs/devel/setup.bash"
 
-export BIG_MODEL_IP="http://94.74.91.114"
-export BIG_MODEL_PORT="30001"
-python3 /home/lib/ramp_detection/main.py
+# export BIG_MODEL_IP="http://94.74.91.114"
+# export BIG_MODEL_PORT="30001"
+# export RUN_FILE=integration_main.py
+python3 $RUN_FILE
